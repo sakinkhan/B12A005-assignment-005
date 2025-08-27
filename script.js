44774f705380c6c4372fbbfbd0bd8d0ecca7ff3d
sakinkhan <sakin2044@gmail.com>
2025-08-28 01:10:10 +1000

    7th Commit - JS modified

--- script.js
+++ script.js
@@ -1,24 +1,84 @@
-console.log('Script.js linked successfully!')
-// On clicking a card's Call Button, following actions will happen:
-// Show an alert with a message including the service name and number
-// Each call will cut 20 coins. Reduce Coin after each click.
-// If coins are less than 20, show a relevant alert and terminate the process.
-// Add this service into the Call History section with:
-// Service name
-// Service number
-
-const callButtons = document.getElementsByClassName('call-btn');
-let serviceNames = document.getElementsByClassName('service-name');
-let serviceNumbers = document.getElementsByClassName('service-number');
-console.log(serviceNames.length, serviceNumbers.length, callButtons.length);
+// Call alert & Coin calculation
+const callButtons = document.getElementsByClassName("call-btn");
+let serviceNames = document.getElementsByClassName("service-name");
+let serviceNumbers = document.getElementsByClassName("service-number");
 
 for (let i = 0; i < callButtons.length; i++) {
-  callButtons[i].addEventListener('click', function () {
-    const name = serviceNames[i] ? serviceNames[i].innerText : 'N/A';
-    const number = serviceNumbers[i] ? serviceNumbers[i].innerText : 'N/A';
-    alert(`${name} and ${number}`);
-    console.log(name, number);
+  callButtons[i].addEventListener("click", function () {
+    const coinCount = parseInt(document.getElementById("coin-count").innerText);
+    const name = serviceNames[i].innerText;
+    const number = serviceNumbers[i].innerText;
+    if (coinCount >= 20) {
+      alert(`📞 Calling ${name} ${number}...`);
+
+      const updatedCoinCount = coinCount - 20;
+      document.getElementById("coin-count").innerText = updatedCoinCount;
+    } else {
+      alert(
+        `❌ A minimum of 20 coins is required to make a call. You don’t have enough coins right now.`
+      );
+      return;
+    }
+
+    // Call History List population
+    const now = new Date();
+    const options = {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    };
+    const currentTime = now.toLocaleTimeString("en-US", options);
+
+    const historyListContainer = document.getElementById(
+      "history-list-container"
+    );
+    const listItem = document.createElement("div");
+    listItem.innerHTML = `
+          <div class="flex flex-row md:flex-col lg:flex-row md:gap-4 items-center justify-between bg-[#FAFAFA] rounded-[12px] p-4 my-2">
+              <div class="sm:text-start md:text-center lg:text-start">
+                  <p class="inter-font font-semibold text-[16px] ">${name}</p>
+                  <p class="hind-madurai-regular text-[16px] text-[#5C5C5C]">${number}</p>
+              </div>
+              <p class="hind-madurai-regular text-[16px]">${currentTime}</p>
+          </div>
+      `;
+    historyListContainer.appendChild(listItem);
   });
 }
 
+// clear history button config
+document.getElementById("clear-btn").addEventListener("click", function () {
+  document.getElementById("history-list-container").innerHTML = "";
+});
 
+// Counting Hearts
+const heartElements = document.getElementsByClassName("fa-heart");
+for (let heart of heartElements) {
+  heart.addEventListener("click", function () {
+    const heartCount = parseInt(
+      document.getElementById("heart-count").innerText
+    );
+    console.log(heartCount);
+    const updatedHeartCount = heartCount + 1;
+    document.getElementById("heart-count").innerText = updatedHeartCount;
+  });
+}
+
+// Calculating copy and alerting number
+const copyButtons = document.getElementsByClassName("copy-btn");
+console.log(copyButtons.length);
+
+for (let i = 0; i < copyButtons.length; i++) {
+  copyButtons[i].addEventListener("click", function () {
+    const number = serviceNumbers[i].innerText;
+    const copyCount = parseInt(document.getElementById("copy-count").innerText);
+    const updatedCopyCount = copyCount + 1;
+    document.getElementById("copy-count").innerText = updatedCopyCount;
+
+    // copy the number to clipboard
+    navigator.clipboard.writeText(number);
+    alert(`The number has been copied: ${number}`);
+    return;
+  });
+}
